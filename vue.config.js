5db07286b78e4f2c16cb73c70c36dc3d420a209c
scronheim <sharko@tbdd.ru>
2021-06-25 11:49:50 +0500

    small fixes

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,42 @@
 module.exports = {
   pluginOptions: {
     electronBuilder: {
+      "build": {
+        "productName": "Radio",
+        "appId": "ru.sconheim.radio",
+        "directories": {
+          "output": "build"
+        },
+        "files": [
+          "dist_electron/bundled/*"
+        ],
+        "publish": [
+          {
+            "provider": "github",
+            "owner": "scronheim",
+            "repo": "radio"
+          }
+        ],
+        "linux": {
+          "publish": [
+            "github"
+          ],
+          "category": "media",
+          "icon": "dist_electron/icon64.png",
+          "target": [
+            "AppImage"
+          ]
+        },
+        "win": {
+          "publish": [
+            "github"
+          ],
+          "icon": "dist_electron/icon64.png",
+          "target": [
+            "nsis"
+          ]
+        }
+      },
       nodeIntegration: true
     }
   },
