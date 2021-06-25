8eaa78e3da95594093f6dd8a663528c4a06e820b
scronheim <sharko@tbdd.ru>
2021-06-25 16:06:22 +0500

    fix markup

--- src/background.js
+++ src/background.js
@@ -75,17 +75,17 @@ ipcMain.handle('read-station-logo', async (event, id) => {
 })
 
 // HACK(mc, 2019-09-10): work around https://github.com/electron-userland/electron-builder/issues/4046
-  if (process.env.DESKTOPINTEGRATION === 'AppImageLauncher') {
-    // remap temporary running AppImage to actual source
-    // THIS IS PROBABLY SUPER BRITTLE AND MAKES ME WANT TO STOP USING APPIMAGE
-    autoUpdater.logger.info('rewriting $APPIMAGE', {
-      oldValue: process.env.APPIMAGE,
-      newValue: process.env.ARGV0,
-    })
-    process.env.APPIMAGE = process.env.ARGV0
-  } else {
-    autoUpdater.logger.info('Not running in AppImageLauncher')
-  }
+if (process.env.DESKTOPINTEGRATION === 'AppImageLauncher') {
+  // remap temporary running AppImage to actual source
+  // THIS IS PROBABLY SUPER BRITTLE AND MAKES ME WANT TO STOP USING APPIMAGE
+  autoUpdater.logger.info('rewriting $APPIMAGE', {
+    oldValue: process.env.APPIMAGE,
+    newValue: process.env.ARGV0,
+  })
+  process.env.APPIMAGE = process.env.ARGV0
+} else {
+  autoUpdater.logger.info('Not running in AppImageLauncher')
+}
 
 autoUpdater.on('update-available', () => {
   win.webContents.send('update_available')
